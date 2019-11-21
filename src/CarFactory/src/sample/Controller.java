feb58aa91934080576a48e3c28f83ffe6980d922
Kacper <v1nkesports@gmail.com>
2019-11-21 22:51:48 +0100

    poczatek layoutu

--- src/CarFactory/src/sample/Controller.java
+++ src/CarFactory/src/sample/Controller.java
@@ -1,12 +1,35 @@
 package sample;
 
+import car.utilities.Car;
+import car.utilities.CarProductionRegion;
+import car.utilities.CarType;
+import factory.CarFactory;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import pubsub.*;
 
 public class Controller {
-    public Label test1;
+    @FXML
+    Label test1;
+    public void buildCar(ActionEvent actionEvent) {
+        PubSubService pubSubService = new PubSubService();
+        Publisher dealer = new PublisherImpl();
+        Publisher engineer = new PublisherImpl();
+        Subscriber client = new SubscriberImpl();
 
-    public void sayHello(ActionEvent actionEvent) {
+        client.addSubscriber("KACPER", pubSubService);
+
+        Message firstStage = new Message("KACPER", "Thanks for buying car");
+        dealer.publish(firstStage, pubSubService);
+
+        Message secondStage = new Message("KACPER", "Production started");
+        engineer.publish(secondStage, pubSubService);
+
+        CarFactory carFactory = new CarFactory();
+        carFactory.setCarProductionRegion(CarProductionRegion.USA);
+        Car car = carFactory.create(CarType.CITY_CAR);
+
         test1.setText("Hello!");
     }
 }
